7ea844edbc46da39b871691a598944d1e913ca79
Dremov <andrew.dremov@gmail.com>
2017-12-12 16:20:01 +0100

    [FIX] fixed bug with storyboard connection

--- FindABuddy/EventTableViewController.swift
+++ FindABuddy/EventTableViewController.swift
@@ -17,6 +17,7 @@ class EventTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Load the sample data.
         loadSampleEvents()
     }
